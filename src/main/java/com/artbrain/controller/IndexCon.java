cb81ce072fee10ea0c0f47c5c8575cd9244a5ea3
SuperNoobTao <387944558@qq.com>
2016-12-06 10:05:23 +0800

    update

--- src/main/java/com/artbrain/controller/IndexCon.java
+++ src/main/java/com/artbrain/controller/IndexCon.java
@@ -30,6 +30,7 @@ public class IndexCon {
 
     /**
      * 进入主页面，如果活动结束，则进入结束页面
+     *
      * @param mav
      * @return
      * @throws ParseException
@@ -48,13 +49,28 @@ public class IndexCon {
 
 
     /**
+     * 活动结束页面
+     *
+     * @param mav
+     * @return
+     * @throws ParseException
+     */
+    @RequestMapping("/finish")
+    public ModelAndView finish(ModelAndView mav) throws ParseException {
+        mav.setViewName("hdjs");
+        return mav;
+    }
+
+
+    /**
      * 进入问题一
+     *
      * @param mav
      * @return
      */
     @RequestMapping("/question1")
     public ModelAndView q1(ModelAndView mav) {
-        mav.addObject("q",1);
+        mav.addObject("q", 1);
         mav.setViewName("index1");
         return mav;
     }
@@ -62,24 +78,26 @@ public class IndexCon {
 
     /**
      * 进入问题二
+     *
      * @param mav
      * @return
      */
     @RequestMapping("/question2")
     public ModelAndView q2(ModelAndView mav) {
-        mav.addObject("q",2);
+        mav.addObject("q", 2);
         mav.setViewName("index2");
         return mav;
     }
 
     /**
      * 进入问题三
+     *
      * @param mav
      * @return
      */
     @RequestMapping("/question3")
     public ModelAndView q3(ModelAndView mav) {
-        mav.addObject("q",3);
+        mav.addObject("q", 3);
         mav.setViewName("index3");
         return mav;
     }
@@ -87,6 +105,7 @@ public class IndexCon {
 
     /**
      * 提交问题一的答案
+     *
      * @param mav
      * @param username
      * @param answer
@@ -96,16 +115,16 @@ public class IndexCon {
     public ModelAndView save1(ModelAndView mav, String username, String answer) {
         int qid = 1;
         if (StringUtils.isEmpty(username) || StringUtils.isEmpty(answer)) {
-            mav.addObject("q",qid);
+            mav.addObject("q", qid);
             mav.setViewName("null");
             return mav;
         }
         Solution solution = new Solution(username, qid, answer);
         if (solutionService.save(solution)) {
-            mav.addObject("q",qid);
+            mav.addObject("q", qid);
             mav.setViewName("success");
         } else {
-            mav.addObject("q",qid);
+            mav.addObject("q", qid);
             mav.setViewName("failed");
         }
         return mav;
@@ -113,6 +132,7 @@ public class IndexCon {
 
     /**
      * 提交问题二的答案
+     *
      * @param mav
      * @param username
      * @param answer
@@ -122,16 +142,16 @@ public class IndexCon {
     public ModelAndView save2(ModelAndView mav, String username, String answer) {
         int qid = 2;
         if (StringUtils.isEmpty(username) || StringUtils.isEmpty(answer)) {
-            mav.addObject("q",qid);
+            mav.addObject("q", qid);
             mav.setViewName("null");
             return mav;
         }
         Solution solution = new Solution(username, qid, answer);
         if (solutionService.save(solution)) {
-            mav.addObject("q",qid);
+            mav.addObject("q", qid);
             mav.setViewName("success");
         } else {
-            mav.addObject("q",qid);
+            mav.addObject("q", qid);
             mav.setViewName("failed");
         }
         return mav;
@@ -139,6 +159,7 @@ public class IndexCon {
 
     /**
      * 提交问题三的答案
+     *
      * @param mav
      * @param username
      * @param answer
@@ -148,16 +169,16 @@ public class IndexCon {
     public ModelAndView save3(ModelAndView mav, String username, String answer) {
         int qid = 3;
         if (StringUtils.isEmpty(username) || StringUtils.isEmpty(answer)) {
-            mav.addObject("q",qid);
+            mav.addObject("q", qid);
             mav.setViewName("null");
             return mav;
         }
         Solution solution = new Solution(username, qid, answer);
         if (solutionService.save(solution)) {
-            mav.addObject("q",qid);
+            mav.addObject("q", qid);
             mav.setViewName("success");
         } else {
-            mav.addObject("q",qid);
+            mav.addObject("q", qid);
             mav.setViewName("failed");
         }
         return mav;
